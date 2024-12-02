24378977480355b8b26649782f686d1632c5d09b
yahya mawhoub <yahyamaw311@gmail.com>
2024-12-01 19:14:02 -0500

    added https

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import { config } from './config/config';
+import https from 'https';
 import connectToDb from './utils/mongodb';
 import app from './app';
+import fs from 'fs';
+import path from 'path';
 
 const port = config.port!;
 
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
-// Démarrer le serveur
-app.listen(port, () => {
+https.createServer(options, app).listen(port, () => {
   connectToDb();
-  console.log(`Serveur en écoute sur <http://localhost>:${port}`);
-});
+  console.log(`Serveur en écoute sur <https://localhost>:${port}`);
+})
