ab7d6d300d1ca9db3ad5162154c21ee3cc48745e
Mathis Normand <2060485@collegemv.qc.ca>
2024-12-11 15:47:38 -0500

    ajout verif

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -111,9 +111,85 @@ export class GameController {
 
     public static async putGame(req: Request, res: Response): Promise<void> {
         const id = req.params.id;
-        const newGameInfo = req.body;
+        const {
+            name,
+            detailed_description,
+            num_vote = 0,
+            score = 0,
+            sample_cover = {
+                height: 0,
+                width: 0,
+                image: '',
+                thumbnail_image: '',
+                platforms: []
+            },
+            genres = [],
+            platforms = []
+        } = req.body;
     
-        await GameService.putGame(newGameInfo, id).then(
+        if (!name || !detailed_description) {
+            res.status(400).send('Nom et description détaillée sont obligatoires');
+            return;
+        }
+    
+        if (isNaN(num_vote) || num_vote < 0) {
+            res.status(400).send('Le nombre de votes doit être un nombre positif');
+            return;
+        }
+    
+        if (isNaN(score) || score < 0) {
+            res.status(400).send('Le score doit être un nombre positif');
+            return;
+        }
+    
+        if (
+            typeof sample_cover.height !== 'number' ||
+            typeof sample_cover.width !== 'number' ||
+            typeof sample_cover.image !== 'string' ||
+            typeof sample_cover.thumbnail_image !== 'string' ||
+            !Array.isArray(sample_cover.platforms)
+        ) {
+            res.status(400).send('La couverture doit contenir les champs requis avec des types valides');
+            return;
+        }
+    
+        if (
+            !Array.isArray(genres) ||
+            genres.some(genre => 
+                !genre.genre_category ||
+                typeof genre.genre_category !== 'string' ||
+                typeof genre.genre_category_id !== 'number' ||
+                typeof genre.genre_id !== 'number' ||
+                typeof genre.genre_name !== 'string'
+            )
+        ) {
+            res.status(400).send('Les genres doivent être un tableau avec des champs valides');
+            return;
+        }
+    
+        if (
+            !Array.isArray(platforms) ||
+            platforms.some(platform =>
+                typeof platform.first_release_date !== 'string' ||
+                typeof platform.platform_id !== 'number' ||
+                typeof platform.platform_name !== 'string'
+            )
+        ) {
+            res.status(400).send('Les plateformes doivent être un tableau avec des champs valides');
+            return;
+        }
+    
+        const updatedGameInfo: GameI = {
+            name,
+            detailed_description,
+            num_vote,
+            score,
+            sample_cover,
+            genres,
+            platforms,
+        };
+    
+        await GameService.putGame(updatedGameInfo, id).then(
             (message) => {
                 switch (message) {
                     case 'Game not found':
@@ -127,9 +203,8 @@ export class GameController {
                         break;
                 }
             }
-        )
+        );
     }
-    
 
     public static async deleteGame(req: Request, res: Response): Promise<void> {    
         const id = req.params.id;
