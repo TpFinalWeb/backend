6074d04feedcca8fe2ab3f6751cfc2831c2ca69e
2060485 <120136410+2060485@users.noreply.github.com>
2024-12-18 16:28:41 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { config } from './config/config';
 import http from 'http';
+import https from 'https';
 import connectToDb from './utils/mongodb.utils';
 import app from './app';
 import fs from 'fs';
@@ -9,7 +10,14 @@ import path from 'path';
 const port = config.port!;
 const serverHttp = config.serverHttp;
 
-http.createServer(app).listen(port, () => {
-  connectToDb();
-  console.log(`Serveur en écoute sur <http://localhost>:${port}`);
-})
+if(serveurHttp){
+  http.createServer(app).listen(port, () => {
+    connectToDb();
+    console.log(`Serveur en écoute sur <http://localhost>:${port}`);
+  })
+}else{
+  https.createServer(app).listen(port, () => {
+    connectToDb();
+    console.log(`Serveur en écoute sur <http://localhost>:${port}`);
+  })
+}
