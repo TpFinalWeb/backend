18212d7b8bfb31b06f95a3c55b67708b8ef86aec
2060485 <2060485@collegemv.qc.ca>
2024-12-18 17:15:05 -0500

    http

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { config } from './config/config';
 import https from 'https';
+import http from 'http';
 import connectToDb from './utils/mongodb.utils';
 import app from './app';
 import fs from 'fs';
@@ -12,7 +13,7 @@ const options = {
   cert: fs.readFileSync('/etc/secrets/cert.pem')
 };
 
-https.createServer(options, app).listen(port, () => {
+http.createServer(app).listen(port, () => {
   connectToDb();
   console.log(`Serveur en écoute sur <https://localhost>:${port}`);
 })
