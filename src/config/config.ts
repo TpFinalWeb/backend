785d243f86ca26812a9572b5687eaf02d72755e8
2060485 <120136410+2060485@users.noreply.github.com>
2024-12-18 16:29:29 -0500

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,5 +6,6 @@ export const config = {
     port: process.env.PORT,
     nodeEnv: process.env.NODE_ENV,
     jwt_secret: process.env.JWT_SECRET,
-    mongo_uri: process.env.MONGO_URI
+    mongo_uri: process.env.MONGO_URI,
+    serverHttp: process.env.serverHttp
 };
