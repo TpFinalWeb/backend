b291ccd87a791bc6a1bc0d5f355cd1f363166ce3
yahya mawhoub <yahyamaw311@gmail.com>
2024-12-18 20:04:58 -0500

    changed geTGames

--- src/models/game.model.ts
+++ src/models/game.model.ts
@@ -13,37 +13,55 @@ const GameSchema = new Schema<GameDocument>({
 
   detailed_description: {
     type: String,
+    required: true,
   },
 
   num_vote: {
     type: Number,
+    required: true,
     min: 0,
   },
 
   score: {
     type: Number,
+    required: true,
     min: 0,
   },
 
   sample_cover: {
     height: {
       type: Number,
-
+      required: true,
     },
     width: {
       type: Number,
-
+      required: true,
     },
     image: {
       type: String,
-
+      required: true,
     },
     thumbnail_image: {
       type: String,
+      required: true,
     },
-    platforms: {
-      type: [String],
-    },
+    platforms: [
+      {
+        _id: false,
+        platform_id: {
+          type: Number,
+          required: true,
+        },
+        platform_name: {
+          type: String,
+          required: true,
+        },
+        first_release_date: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
   },
 
   genres: [
@@ -51,19 +69,19 @@ const GameSchema = new Schema<GameDocument>({
       _id: false,
       genre_category: {
         type: String,
-  
+        required: true,
       },
       genre_category_id: {
         type: Number,
-  
+        required: true,
       },
       genre_id: {
         type: Number,
-  
+        required: true,
       },
       genre_name: {
         type: String,
-  
+        required: true,
       },
     },
   ],
@@ -73,15 +91,15 @@ const GameSchema = new Schema<GameDocument>({
       _id: false,
       platform_id: {
         type: Number,
-  
+        required: true,
       },
       platform_name: {
         type: String,
-  
+        required: true,
       },
       first_release_date: {
         type: String,
-  
+        required: true,
       },
     },
   ],
